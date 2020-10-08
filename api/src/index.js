af9167798377f66583164652679b5c323115c981
Illiya Chernyak <Illiya.Chernyak@outlook.com>
2020-10-08 18:00:21 +0300

    env.var add

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,17 @@
 const express = require("express")
 const app = express()
+const port = process.env.PORT
+const host = process.env.HOST
+
+app.get("/", (req, res) =>{
+    res.send("main is OK")
+})
 
 app.get("/test", (req, res) =>{
     res.send("all OK")
 })
 
-app.listen(3000, () => {
-    console.log("api is working")
+app.listen(port, () => {
+    console.log(`api is working on port ${port}`)
+    console.log(`hostis ${host}`)
 })
